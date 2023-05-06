ecfff1b4802cffb939a796a97e9bf574fb67e557
Egentiann <81017974+adayemi@users.noreply.github.com>
2023-05-06 06:23:46 +0100

    Delete sqlgg.sql

--- sqlgg.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE customers(
-  customer_id varchar(3),
-  first_name varchar(50),
-  last_name varchar(50),
-  gender char(1),
-  birth_date date,
-  phone varchar(20),
-  email varchar(50),
-  addresss varchar (100));
